97821bd8fdfb7f382931d9cfe727b8d77cd6848c
liu <8782687+l1634220259@user.noreply.gitee.com>
2023-01-28 06:18:00 +0000

    update 偏移，礼包.lua.
    Signed-off-by: liu <8782687+l1634220259@user.noreply.gitee.com>

--- 偏移，礼包.lua
+++ 偏移，礼包.lua
@@ -66,10 +66,10 @@ qygdz = rwdz+0xFFDD07B
  huaxing = rwdz+0x102E5648
  jingtou = rwdz+0x107DB76C
  xianzuguang = rwdz+0x10596F38
- csaddr =  sddz+0x16107A8
- dtaddr =  sddz+0x22ECF50
+
+ 
  mrrw =  sddz+0x229F2C
- ydgypy =  sddz+0x1B4CA6C
+ 
  xjdiaoxiang =  sddz+0x1B4D89C
  xhdz =  sddz+0x21E7704
  Lonsd =  sddz+0x107C1C0
