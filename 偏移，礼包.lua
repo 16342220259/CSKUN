5bb1efac9f49a03b6c7078da90e2ffef3edf04a1
liu <8782687+l1634220259@user.noreply.gitee.com>
2023-03-09 11:46:51 +0000

    update 偏移，礼包.lua.
    Signed-off-by: liu <8782687+l1634220259@user.noreply.gitee.com>

--- 偏移，礼包.lua
+++ 偏移，礼包.lua
@@ -5,9 +5,9 @@ sooo=gg.getRangesList('libBootloader.so:bss')[1].start  --cb
 soo=so+0x15cd000 --cd
 sddz=qi(soo,0x13c48)+0x1C8 
 rw1 = sooo
-rw2=qi(rw1,0x8cbc8)+0x10
+rw2=qi(rw1,0x8cc68)+0x10
 rw3=qi(rw2,0)+0x30
-rw4=qi(rw3,0)+0x1ec
+rw4=qi(rw3,0)+0x1fc
 rwdz=rw4
 --
 ------------------------------
