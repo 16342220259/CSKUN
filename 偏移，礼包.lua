177457e2db329f4316ef7aefa282b70cdfa76a1c
16342220259 <59324757+16342220259@users.noreply.github.com>
2023-09-29 23:42:24 +0800

    Update 偏移，礼包.lua

--- 偏移，礼包.lua
+++ 偏移，礼包.lua
@@ -2,10 +2,10 @@ function PY0()
  
 so=gg.getRangesList('libBootloader.so')[1].start  --xa 
 sooo=gg.getRangesList('libBootloader.so:bss')[1].start  --cb  
-soo=so+0x17b9000 --cd
-sddz=qi(soo,0x15900)+0x1D4 
+soo=so+0x17cb000 --cd
+sddz=qi(soo,0x15b80)+0x1D4 
 rw1 = sooo
-rw2=qi(rw1,0x934a8)+0x10
+rw2=qi(rw1,0x92e68)+0x10
 rw3=qi(rw2,0)+0x30
 rw4=qi(rw3,0)+0x26c
 rwdz=rw4         -----3057Q没有就3000-4000Q111111111111
