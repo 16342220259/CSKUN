6fc9939a047d0d387cb406072b80e323229935f2
16342220259 <59324757+16342220259@users.noreply.github.com>
2024-01-08 21:15:40 +0800

    Update 偏移，礼包.lua

--- 偏移，礼包.lua
+++ 偏移，礼包.lua
@@ -1,9 +1,12 @@
 function PY0()
 
  
-so=gg.getRangesList('libBootloader.so')[1].start  --xa 
+pso=gg.getRangesList('libBootloader.so')[1].start  --xa 
 sooo=gg.getRangesList('libBootloader.so:bss')[1].start  --cb  
-soo=so+0x7c4000 --cd
+psoo=so+0x7c4000 --cd
+so = psoo
+ soo = pso
+
 sddz=qi(soo,0x16BA8)+0x23C
 rw1 = sooo
 rw2=qi(rw1,0x96a20)+0x10
