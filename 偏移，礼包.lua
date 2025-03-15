7490cf5b82ecfcda790210c39486a5d85e97f091
16342220259 <59324757+16342220259@users.noreply.github.com>
2025-03-15 16:35:45 +0800

    更新 偏移，礼包.lua

--- 偏移，礼包.lua
+++ 偏移，礼包.lua
@@ -9,9 +9,10 @@ soo=so+0x1422000 --cd 0x7d6000  0x7ce000  0x7cd000
 
 rw1 = sooo
 sddz=qi(rw1,0x405DF8)+0xD8
-lzjs1=qi(rw1,0x44d5d8)+0x98
-lzjs2=qi(lzjs1,0)+0x1d4
-lzjs=lzjs2    
+lzjs1=qi(rw1,0x12c370)+0x10
+lzjs2=qi(lzjs1,0)+0x98
+lzjs3=qi(lzjs1,0)+0x1d4
+lzjs=lzjs3
 rw2=qi(rw1,0xad668)+0x10
 rw3=qi(rw2,0)+0x30
 rw4=qi(rw3,0)+0x29c
@@ -142,9 +143,10 @@ libao={
   {'SNC121'},{'SNC122'},{'SNC123'},{'SNC124'},
   {'SNC125'}, {'SNC126'},{'SNC127'},{'SNC128'},
   {'SNC129'}, {'SNC130'},{'SNC131'},{'SNC132'},
-  {'SNC133'}, {'SNC134'},{'SNC139'},{'SNC140'},
-{'SNC141'},{'SNC142'},{'SNC134'},{'SNC143'},{'SNC144'},
-{'BSNC19'},{'BSNC14'},
+  {'SNC133'}, {'SNC134'},{'SNC136'},{'SNC137'},
+  {'SNC138'},{'SNC139'}，{'SNC139'},{'SNC140'},
+{'SNC141'},{'SNC142'},{'SNC143'},{'SNC144'},
+{'BSNC19'},{'BSNC14'},{'BSNC18'},
  
  
  
